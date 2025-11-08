f0159864619a85157b693cc82ecd6934688d6fab
Alfirajib <alfirajib7@gmail.com>
2025-11-08 11:58:24 +0700

    Test backend connection

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
